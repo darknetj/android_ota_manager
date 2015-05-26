82999e7b023c70eaa1b3aa4a61cdbdd231aa712d
dmix <dan@dmix.is>
2015-05-26 17:29:28 -0400

    Compute md5 checksum for each file

--- models/files.go
+++ models/files.go
@@ -4,13 +4,20 @@ import (
     "log"
     "fmt"
     "strings"
+    "os"
+    "io"
+    "encoding/hex"
+    "crypto/md5"
     "io/ioutil"
     "github.com/rakyll/magicmime"
 )
 
+var buildFiles []File
+
 type File struct {
     Name string
     Size int64
+    Md5 string
 }
 
 func (b *File) DownloadUrl() string {
@@ -24,39 +31,47 @@ func (b *File) DeleteUrl() string {
 }
 
 func Files() []File {
-    files, _ := ioutil.ReadDir(BuildsPath)
+    /* files, _ := ioutil.ReadDir(BuildsPath) */
     buildFiles := make([]File, 0)
-    for _, f := range files {
-        file := File{f.Name(), f.Size()}
-        if strings.Contains(f.Name(), "zip") {
-            buildFiles = append(buildFiles, file)
-        }
-        fmt.Println(file)
-    }
-    fmt.Println(buildFiles)
+    // for _, f := range files {
+    //     file := File{f.Name(), f.Size()}
+    //     if strings.Contains(f.Name(), "zip") {
+    //         buildFiles = append(buildFiles, file)
+    //     }
+    //     fmt.Println(file)
+    // }
+    // fmt.Println(buildFiles)
     return buildFiles
 }
 
 func ProcessFiles() {
-    var buildFiles []File
+    var fileList []File
     mm,_ := magicmime.New(magicmime.MAGIC_MIME_TYPE | magicmime.MAGIC_SYMLINK | magicmime.MAGIC_ERROR)
     files, _ := ioutil.ReadDir(BuildsPath)
     for _, f := range files {
-        filepath := strings.Join([]string{BuildsPath, f.Name()}, "")
+        filepath := strings.Join([]string{BuildsPath, f.Name()}, "/")
         mimetype,_ := mm.TypeByFile(filepath)
-        log.Println(mimetype)
-        if mimetype == "application/zip" {
+        if mimetype == "application/java-archive" {
             file := File{
                 Name: f.Name(),
                 Size: f.Size(),
+                Md5: Md5File(filepath),
             }
-            buildFiles = append(buildFiles, file)
-            log.Println(file)
+            fileList = append(fileList, file)
+        } else {
+            log.Println("File skipped", mimetype)
         }
     }
-    fmt.Println(buildFiles)
+    buildFiles = fileList
 
     // TODO: Prune DB for old files
     // loop through each in db
     // check if file still exists, if not delete from db
+}
+
+func Md5File(filepath string) string {
+	h, _ := os.Open(filepath)
+	buf := md5.New()
+	io.Copy(buf, h)
+  return hex.EncodeToString(buf.Sum(nil))
 }
