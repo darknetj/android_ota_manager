12b8f754c7635c51adf95aa273ae8a88cb285a76
Kevin C. Krinke <kevin@krinke.ca>
2015-12-08 00:13:00 -0500

    make installer work on clean systems

--- installer.sh
+++ installer.sh
@@ -19,7 +19,7 @@ echo "> Copying binary to /usr/local/bin"
 cp android_ota_manager /usr/local/bin/
 
 echo "> Creating $appPath"
-mkdir $appPath
+mkdir -pv $appPath
 echo "> Touching $dbPath"
 touch $dbPath
 
@@ -28,12 +28,21 @@ cp -rf ./views $appPath/views
 cp -f ./config.yml $appPath/config.yml
 
 echo "> Creating build directories to $buildsPath"
-mkdir $buildsPath/builds
-mkdir $buildsPath/builds/deleted
-mkdir $buildsPath/builds/published
+mkdir -pv $buildsPath/builds
+mkdir -pv $buildsPath/builds/deleted
+mkdir -pv $buildsPath/builds/published
+
+echo "> Installing systemd service"
+cp -v android_ota_manager.service /etc/systemd/system/android_ota_manager.service
+systemctl enable android_ota_manager.service
+
+echo "> Adding android_ota_manager user"
+useradd android_ota_manager -s /bin/false
+groupadd storage 
+usermod -g storage android_ota_manager
 
 echo "> Setting permissions"
-chmod 555 /usr/local/bin/android_ota_manager 
+chmod 555 /usr/local/bin/android_ota_manager
 
 chmod -R 0770 $dbPath
 chmod -R 0770 $appPath
@@ -42,15 +51,6 @@ chmod -R 0770 $buildsPath
 chown -R android_ota_manager:storage $dbPath
 chown -R android_ota_manager:storage $appPath
 chown -R uploader:storage $buildsPath/builds
-
-echo "> Installing systemd service"
-cp android_ota_manager.service /etc/systemd/system/android_ota_manager.service
-systemctl enable android_ota_manager.service
-
-echo "> Adding android_ota_manager user"
-useradd android_ota_manager -s /bin/false
-groupadd storage 
-usermod -g storage android_ota_manager
 
 echo "> Starting android_ota_manager"
 systemctl enable android_ota_manager.service
